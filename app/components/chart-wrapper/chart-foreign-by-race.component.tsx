0421ad39b4996b6dc47927c9c702d66835fe9d11
matisantillan11 <matias1.santillan@gmail.com>
2025-01-20 09:06:07 -0300

    Add ChartForeignByRace component to visualize foreign student enrollments by race

---
+++ app/components/chart-wrapper/chart-foreign-by-race.component.tsx
@@ -0,0 +1,33 @@
+import { Bar } from 'recharts'
+import { BarChart } from '../ui'
+import { useDatafetchingContext } from '~/app/context/data-fetching.context'
+import { getTotalForeignPopulation, sumAllEnrolledRaces } from './utils'
+import { formatNumberToLabel } from '~/app/utils'
+
+export default function ChartForeignByRace() {
+	const { extraDetails, filters } = useDatafetchingContext()
+
+	const percentageOfEnrolledForeignCitizens = (
+		(sumAllEnrolledRaces(extraDetails?.enrollments) /
+			getTotalForeignPopulation(extraDetails?.foreignPopulationByRace)) *
+		100
+	).toFixed(0)
+
+	return (
+		<div className='px-20'>
+			<div className='w-full my-10 px-2'>
+				<h2 className='font-bold'>Enrollments by race</h2>
+				<p>
+					In {filters?.year} there were {formatNumberToLabel(sumAllEnrolledRaces(extraDetails?.enrollments), true)}{' '}
+					students enrolled in the United States, this number represents {percentageOfEnrolledForeignCitizens}% of the
+					foreing citizens.
+				</p>
+			</div>
+
+			<BarChart xAxisDataKey='race' data={extraDetails?.enrollments}>
+				<Bar dataKey='enrollment_women' name='Women enrolled' fill='#ce0e0e' />
+				<Bar dataKey='enrollment_male' name='Men enrolled' fill='#1453BC' />
+			</BarChart>
+		</div>
+	)
+}

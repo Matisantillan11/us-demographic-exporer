211b5ffed2d935595bf2538926262a5e4a84b80b
matisantillan11 <matias1.santillan@gmail.com>
2025-01-20 09:47:24 -0300

    Enhance DataTable component with pagination controls and improve structure

--- app/components/ui/table/data-table.component.tsx
+++ app/components/ui/table/data-table.component.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table'
+import { ColumnDef, flexRender, getCoreRowModel, getPaginationRowModel, useReactTable } from '@tanstack/react-table'
 import { Table, TableBody, TableCell, TableHead, TableRow } from './body'
 import { TableHeader } from './header'
+import { Button } from '../button'
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[]
@@ -13,40 +14,52 @@ export default function DataTable<TData, TValue>({ columns, data }: DataTablePro
 		data,
 		columns,
 		getCoreRowModel: getCoreRowModel(),
+		getPaginationRowModel: getPaginationRowModel(),
 	})
 
 	return (
-		<Table>
-			<TableHeader>
-				{table?.getHeaderGroups()?.map((headerGroup) => (
-					<TableRow key={headerGroup.id}>
-						{headerGroup.headers.map((header) => {
-							return (
-								<TableHead key={header.id}>
-									{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-								</TableHead>
-							)
-						})}
-					</TableRow>
-				))}
-			</TableHeader>
-			<TableBody>
-				{table && table.getRowModel().rows ? (
-					table.getRowModel().rows.map((row) => (
-						<TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
-							{row.getVisibleCells().map((cell) => (
-								<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
-							))}
+		<div>
+			<Table>
+				<TableHeader>
+					{table?.getHeaderGroups()?.map((headerGroup) => (
+						<TableRow key={headerGroup.id}>
+							{headerGroup.headers.map((header) => {
+								return (
+									<TableHead key={header.id}>
+										{header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
+									</TableHead>
+								)
+							})}
 						</TableRow>
-					))
-				) : (
-					<TableRow>
-						<TableCell colSpan={columns?.length} className='h-24 text-center'>
-							No results.
-						</TableCell>
-					</TableRow>
-				)}
-			</TableBody>
-		</Table>
+					))}
+				</TableHeader>
+				<TableBody>
+					{data?.length ? (
+						table.getRowModel().rows.map((row) => (
+							<TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
+								{row.getVisibleCells().map((cell) => (
+									<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+								))}
+							</TableRow>
+						))
+					) : (
+						<TableRow>
+							<TableCell colSpan={columns?.length} className='h-24 text-center'>
+								No results.
+							</TableCell>
+						</TableRow>
+					)}
+				</TableBody>
+			</Table>
+
+			<div className='flex items-center justify-end space-x-2 py-4'>
+				<Button variant='ghost' size='sm' onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+					Previous
+				</Button>
+				<Button variant='ghost' size='sm' onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+					Next
+				</Button>
+			</div>
+		</div>
 	)
 }
